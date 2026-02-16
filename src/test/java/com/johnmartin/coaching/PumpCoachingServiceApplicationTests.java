f1d4974a685014a67eff589f3fafd26f3dd26228
John Martin Marasigan <marasiganjohnmartin@gmail.com>
2026-02-17 04:31:26 +0800

    initial commit

---
+++ src/test/java/com/johnmartin/coaching/PumpCoachingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnmartin.coaching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PumpCoachingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
